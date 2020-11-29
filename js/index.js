9f31cffbf6c8a634451c4c7043a157081b66e2b3
Luke Haines <lukehaines2@gmail.com>
2020-11-29 23:52:54 +0000

    add entire countdown class

--- js/index.js
+++ js/index.js
@@ -1,15 +1,63 @@
-const time = 150000;
-
-const minutes = Math.floor(time / 1000 / 60);
-console.log('L5 === minutes', minutes);
-
-const seconds = Math.floor(time / 1000) % 60;
-console.log('L8 === seconds', seconds);
-
 class CountDown {
   constructor(expiredDate, onRender, onComplete) {
+    this.setExpiredDate(expiredDate);
+
     this.onRender = onRender;
     this.onComplete = onComplete;
-    // Todo: Handle expired
+  }
+
+  setExpiredDate(expiredDate) {
+    // get the current time
+    const currentTime = new Date().getTime();
+
+    // calculate the remaining time 
+    this.timeRemaining = expiredDate.getTime() - currentTime;
+
+    this.timeRemaining <= 0 ?
+      this.complete() :
+      this.start();
+  }
+
+
+  complete() {
+    if (typeof this.onComplete === 'function') {
+      onComplete();
+    }
+  }
+  getTime() {
+    return {
+      days: Math.floor(this.timeRemaining / 1000 / 60 / 60 / 24),
+      hours: Math.floor(this.timeRemaining / 1000 / 60 / 60) % 24,
+      minutes: Math.floor(this.timeRemaining / 1000 / 60) % 60,
+      seconds: Math.floor(this.timeRemaining / 1000) % 60
+    };
+  }
+
+  update() {
+    if (typeof this.onRender === 'function') {
+      this.onRender(this.getTime());
+    }
+  }
+
+  start() {
+    // update the countdown
+    this.update();
+
+    //  setup a timer
+    const intervalId = setInterval(() => {
+      // update the timer  
+      this.timeRemaining -= 1000;
+
+      if (this.timeRemaining < 0) {
+        // call the callback
+        complete();
+
+        // clear the interval if expired
+        clearInterval(intervalId);
+      } else {
+        this.update();
+      }
+
+    }, 1000);
   }
 }
