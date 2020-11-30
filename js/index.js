6d57520537e34386a9bbea3b2a2c59a628b23edc
Luke Haines <lukehaines2@gmail.com>
2020-11-30 23:38:41 +0000

    add app functionality

--- js/index.js
+++ js/index.js
@@ -1,63 +1,71 @@
-class CountDown {
-  constructor(expiredDate, onRender, onComplete) {
-    this.setExpiredDate(expiredDate);
+// Get the new year 
+const getNewYear = () => {
+  const currentYear = new Date().getFullYear();
+  return new Date(`January 01 ${currentYear + 1} 00:00:00`);
+};
 
-    this.onRender = onRender;
-    this.onComplete = onComplete;
-  }
+// update the year element
+const year = document.querySelector('.year');
+year.innerHTML = getNewYear().getFullYear();
 
-  setExpiredDate(expiredDate) {
-    // get the current time
-    const currentTime = new Date().getTime();
-
-    // calculate the remaining time 
-    this.timeRemaining = expiredDate.getTime() - currentTime;
-
-    this.timeRemaining <= 0 ?
-      this.complete() :
-      this.start();
-  }
+// select elements
+const app = document.querySelector('.countdown-timer');
+const message = document.querySelector('.message');
+const heading = document.querySelector('h1');
 
 
-  complete() {
-    if (typeof this.onComplete === 'function') {
-      onComplete();
-    }
-  }
-  getTime() {
-    return {
-      days: Math.floor(this.timeRemaining / 1000 / 60 / 60 / 24),
-      hours: Math.floor(this.timeRemaining / 1000 / 60 / 60) % 24,
-      minutes: Math.floor(this.timeRemaining / 1000 / 60) % 60,
-      seconds: Math.floor(this.timeRemaining / 1000) % 60
-    };
-  }
+const format = (t) => {
+  return t < 10 ? '0' + t : t;
+};
 
-  update() {
-    if (typeof this.onRender === 'function') {
-      this.onRender(this.getTime());
-    }
-  }
+const render = (time) => {
+  app.innerHTML = `
+      <div class="count-down">
+          <div class="timer">
+              <h2 class="days">${format(time.days)}</h2>
+              <small>Days</small>
+          </div>
+          <div class="timer">
+              <h2 class="hours">${format(time.hours)}</h2>
+              <small>Hours</small>
+          </div>
+          <div class="timer">
+              <h2 class="minutes">${format(time.minutes)}</h2>
+              <small>Minutes</small>
+          </div>
+          <div class="timer">
+              <h2 class="seconds">${format(time.seconds)}</h2>
+              <small>Seconds</small>
+          </div>
+      </div>
+      `;
+};
 
-  start() {
-    // update the countdown
-    this.update();
 
-    //  setup a timer
-    const intervalId = setInterval(() => {
-      // update the timer  
-      this.timeRemaining -= 1000;
+const showMessage = () => {
+  message.innerHTML = `Happy New Year ${newYear}!`;
+  app.innerHTML = '';
+  heading.style.display = 'none';
+};
 
-      if (this.timeRemaining < 0) {
-        // call the callback
-        complete();
+const hideMessage = () => {
+  message.innerHTML = '';
+  heading.style.display = 'block';
+};
 
-        // clear the interval if expired
-        clearInterval(intervalId);
-      } else {
-        this.update();
-      }
+const complete = () => {
+  showMessage();
 
-    }, 1000);
-  }
-}
+  // restart the countdown after showing the 
+  // greeting message for a day ()
+  setTimeout(() => {
+    hideMessage();
+    countdownTimer.setExpiredDate(getNewYear());
+  }, 1000 * 60 * 60 * 24);
+};
+
+const countdownTimer = new CountDown(
+  getNewYear(),
+  render,
+  complete
+);
